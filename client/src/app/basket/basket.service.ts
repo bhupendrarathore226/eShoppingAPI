511783b47c75d375f074532f406bd1c5888b626d
bhupendrarathore226 <bhupendra.rathore226@gmail.com>
2026-03-03 00:12:59 -0500

    updated to point gateway

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -10,7 +10,7 @@ import { IProduct } from '../shared/models/product';
   providedIn: 'root'
 })
 export class BasketService {
-  baseUrl = 'https://localhost:9010';
+  baseUrl = 'https://localhost:51341';
   constructor(private http: HttpClient, private acntService: AcntService, private router: Router) { }
   private basketSource = new BehaviorSubject<Basket | null>(null);
   basketSource$ = this.basketSource.asObservable();
--- client/src/app/store/store.service.ts
+++ client/src/app/store/store.service.ts
@@ -13,7 +13,7 @@ export class StoreService {
 
   constructor(private http: HttpClient) { }
 
-  baseUrl = 'https://localhost:9010/';
+  baseUrl = 'https://localhost:51341/';
 
   getProductById(id:string){
     return this.http.get<IProduct>(this.baseUrl + 'Catalog/GetProductById/' +id );
